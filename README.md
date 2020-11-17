dc053e542875320de2c3e87829ce4507fcf5a5d5
sushi <sushi@rainbowism.com>
2020-11-17 15:47:30 -0500

    Add context menu to the map icon

--- README.md
+++ README.md
@@ -6,14 +6,14 @@ A simple entrance randomizer tracker. There is no logic for tracking accessible 
 
 ## Usage
 
+* Map in the bottom right is an options menu.
 * Right clicking markers hides them.
-* Click markers to select, and click on the map to place.
-* Right clicking the map in the bottom right clears all overworld item markers.
+* Click markers to select, and click (or drag) on the map to place.
 * Hovering over a connector will draw lines to all linked connectors.
 * Right clicking a dungeon notes button in the bottom right will increment the check counter.
 * Clicking on a marker while dungeon notes are open will let you make a path to get to that check.
-* Ctrl+Z to unhide markers
+* Ctrl+Z to unhide markers, up to 10.
 
 This is built on [Godot 3](https://godotengine.org) if you want to make changes or add networking to it.
 
-cool
+cool!
--- src/GUI/GUI.gd
+++ src/GUI/GUI.gd
@@ -1,12 +1,25 @@
 extends Control
 
+enum {
+	MENU_OW_ITEMS = 0,
+	MENU_DRAG_N_DROP = 1,
+	MENU_RESET = 100
+}
+
 onready var marker_scene: PackedScene = preload("res://src/Objects/Marker.tscn")
+onready var menu = $PopupMenu
 onready var notes_modal = $Container/Notes/Shadow
 onready var notes_container = $Container/Notes/Shadow/Container/BG
 
 func _ready() -> void:
 	Events.connect("marker_clicked", self, "generate_marker")
 	Events.connect("notes_clicked", self, "open_notes")
+	menu.connect("id_pressed", self, "menu_pressed")
+
+	menu.add_item("Hide OW Item Markers", MENU_OW_ITEMS)
+	menu.add_check_item("Drag n' Drop Markers", MENU_DRAG_N_DROP)
+	menu.add_separator()
+	menu.add_item("!!RESET!!", MENU_RESET)
 
 func generate_marker(texture: Texture, color: Color, connector: String) -> void:
 	if Util.mode != Util.MODE_OW:
@@ -26,3 +39,18 @@ func open_notes(node: Node) -> void:
 		notes_container.remove_child(child)
 	notes_container.add_child(node)
 	notes_modal.show()
+
+func open_menu() -> void:
+	menu.popup()
+	menu.rect_global_position = get_global_mouse_position() - menu.rect_size
+
+func menu_pressed(id: int) -> void:
+	if menu.is_item_checkable(id):
+		menu.set_item_checked(id, !menu.is_item_checked(id))
+	match(id):
+		MENU_OW_ITEMS:
+			get_tree().call_group("item", "queue_free")
+		MENU_DRAG_N_DROP:
+			Util.drag_and_drop = menu.is_item_checked(id)
+		MENU_RESET:
+			get_tree().reload_current_scene()
